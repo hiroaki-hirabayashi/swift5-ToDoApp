0d0ff511900fa7b8d81ced53a723591ae51cacfa
Hiroaki Hirabayashi <h-hirabayashi@rough-and-ready.co.jp>
2020-12-14 01:50:45 +0900

    Combine内のsinkの処理をassignに置き換え変更

--- Swift5 Todo/Controller/EditViewController.swift
+++ Swift5 Todo/Controller/EditViewController.swift
@@ -24,6 +24,7 @@ final class EditViewController: UIViewController {
     @IBOutlet weak var todoTextField: UITextField!
     @IBOutlet weak var todoUpdateButton: UIButton!
     private var didChangeCancellable: AnyCancellable? //Combineを使ってNotificationを受け取る
+    private var didChangeButtonColorCancellable: AnyCancellable?
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
@@ -31,14 +32,31 @@ final class EditViewController: UIViewController {
         
         todoTextField.text = editTodo.text
         
+//        didChangeCancellable = NotificationCenter.default
+//            .publisher(for: UITextField.textDidChangeNotification, object: todoTextField)
+//            .compactMap { $0.object as? UITextField } //notification.objectがUITextFieldにキャストできるものだけ次に進む
+//            .compactMap { $0.text?.isEmpty ?? false } //上から渡されたTextFieldが空かどうか
+//            .sink(receiveValue: { isInputTextEmpty in //TextFieldに入力された文字が空かどうかが渡される
+//                self.todoUpdateButton.isEnabled = self.todoTextField.text?.isEmpty ?? true ? false : true
+//                self.todoUpdateButton.backgroundColor = self.todoTextField.text?.isEmpty ?? true ? .gray : .systemOrange
+//            })
+        
         didChangeCancellable = NotificationCenter.default
             .publisher(for: UITextField.textDidChangeNotification, object: todoTextField)
             .compactMap { $0.object as? UITextField } //notification.objectがUITextFieldにキャストできるものだけ次に進む
-            .compactMap { $0.text?.isEmpty ?? false } //上から渡されたTextFieldが空かどうか
-            .sink(receiveValue: { isInputTextEmpty in //TextFieldに入力された文字が空かどうかが渡される
-                self.todoUpdateButton.isEnabled = self.todoTextField.text?.isEmpty ?? true ? false : true
-                self.todoUpdateButton.backgroundColor = self.todoTextField.text?.isEmpty ?? true ? .gray : .systemOrange
-            })
+            .compactMap { $0.text!.count >= 1 } //上から渡されたTextField.textの文字数が1文字以上か
+            .assign(to: \.isEnabled, on: todoUpdateButton)
+       
+        didChangeButtonColorCancellable = NotificationCenter.default
+               .publisher(for: UITextField.textDidChangeNotification, object: todoTextField)
+               .compactMap { $0.object as? UITextField }
+               .compactMap { $0.text?.isEmpty ?? false }
+               .sink(receiveValue: { (isInputTextEmpty) in
+                self.todoUpdateButton.backgroundColor = isInputTextEmpty ? .gray : .systemIndigo
+               })
+
+
+        
     }
     // MARK: - function
     @IBAction func tapEditButton(_ sender: Any) {
