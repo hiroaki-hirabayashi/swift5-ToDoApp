396be2cff1b8b9f81eaa0ece7b786afaa3b7142e
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2020-12-15 01:02:57 +0900

    Combineの処理をsinkからassignに変更

--- Swift5 Todo/Controller/EditViewController.swift
+++ Swift5 Todo/Controller/EditViewController.swift
@@ -24,7 +24,7 @@ final class EditViewController: UIViewController {
     @IBOutlet weak var todoTextField: UITextField!
     @IBOutlet weak var todoUpdateButton: UIButton!
     private var didChangeCancellable: AnyCancellable? //Combineを使ってNotificationを受け取る
-    
+    private var didChangeButtonColorCancellable: AnyCancellable?
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
@@ -32,21 +32,32 @@ final class EditViewController: UIViewController {
         
         todoTextField.text = editTodo.text
         
+//        didChangeCancellable = NotificationCenter.default
+//            .publisher(for: UITextField.textDidChangeNotification, object: todoTextField)
+//            .sink(receiveValue: { (notification) in
+//                if let editText = notification.object as? UITextField {
+//                    if let editTodo = editText.text {
+//                        if editTodo.isEmpty ?? true {
+//                            self.todoUpdateButton.isEnabled = false
+//                            self.todoUpdateButton.backgroundColor = .gray
+//                        } else {
+//                            self.todoUpdateButton.isEnabled = true
+//                            self.todoUpdateButton.backgroundColor = .orange
+//                        }
+//                    }
+//                }
+//            })
         didChangeCancellable = NotificationCenter.default
             .publisher(for: UITextField.textDidChangeNotification, object: todoTextField)
-            .sink(receiveValue: { (notification) in
-                if let editText = notification.object as? UITextField {
-                    if let editTodo = editText.text {
-                        if editTodo.isEmpty ?? true {
-                            self.todoUpdateButton.isEnabled = false
-                            self.todoUpdateButton.backgroundColor = .gray
-                        } else {
-                            self.todoUpdateButton.isEnabled = true
-                            self.todoUpdateButton.backgroundColor = .orange
-                        }
-                    }
-                }
-            })
+            .compactMap { $0.object as? UITextField } //notification.objectがUITextFieldにキャストできるものだけ次に進む
+            .compactMap { $0.text?.count ?? 1 >= 1 } //上から渡されたTextField.textの文字数が1文字以上か
+            .assign(to: \.isEnabled, on: todoUpdateButton)
+        
+        didChangeButtonColorCancellable = NotificationCenter.default
+            .publisher(for: UITextField.textDidChangeNotification, object: todoTextField)
+            .compactMap { $0.object as? UITextField }
+            .compactMap { $0.text?.isEmpty ?? true ? .gray : .systemPurple }
+            .assign(to: \.backgroundColor, on: todoUpdateButton)
         
     }
     // MARK: - function
